921eb93c0ff41f53a26eaa0490377427be2a989c
Trekzest <saurabhkumarsingh.work@outlook.com>
2026-01-02 18:27:15 +0530

    day

---
+++ day.py
@@ -0,0 +1,27 @@
+#Get input
+"""
+name = input("what is your name? ")
+color = input("what is your faviorate color? ")
+print(f"{name} likes {color}")
+"""
+#Type conversion
+"""
+weight_lbs = input("Enter your weight in pounds: ")
+weight_kg = float(weight_lbs) * 0.45
+print(f"your weight is {weight_kg:.2f} Kilograms")
+"""
+#string - Multiline string
+"""
+course = '''
+Hi john,
+Here is our first email to you.
+Thank you
+The support team
+'''
+"""
+#string method
+course = "python for beginners"
+print(len(course))
+print(course.upper())
+print(course)
+print(course.find('p'))
